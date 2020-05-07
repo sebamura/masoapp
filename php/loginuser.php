0b040fafcb096e25895be524d90f828408bdfe42
Said Ravest <said.tattoo@gmail.com>
2020-05-07 11:30:07 -0400

    comandos en txt

--- php/loginuser.php
+++ php/loginuser.php
@@ -1,10 +1,8 @@
 <?php
-
+	include "conexion.php";
 if(!empty($_POST)){
 	if(isset($_POST["username"]) &&isset($_POST["password"])){
 		if($_POST["username"]!=""&&$_POST["password"]!=""){
-			include "conexion.php";
-			
 			$user_id=null;
 			$sql1= "select * from user where (username=\"$_POST[username]\" or email=\"$_POST[username]\") and password=\"$_POST[password]\" ";
 			$query = $con->query($sql1);
@@ -17,7 +15,7 @@ if(!empty($_POST)){
 			}else{
 				session_start();
 				$_SESSION["user_id"]=$user_id;
-				print "<script>window.location='../home.php';</script>";				
+				print "<script>window.location='../home.php';</script>";
 			}
 		}
 	}
